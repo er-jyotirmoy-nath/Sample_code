8e3a8eda0a5a771bc81102d4e1e0276f1020067d
Jyotirmoy Nath <er.jyotirmoy.nath@gmail.com>
2017-04-23 10:23:57 +0530

    Create heetp_service.js

---
+++ heetp_service.js
@@ -0,0 +1,7 @@
+$http.get(‘/someUrl’, config).then(successCallback, errorCallback);
+
+$http.post(‘/someUrl’, data, config).then(successCallback, errorCallback);
+
+$http.put(‘/someUrl’, data, config).then(successCallback, errorCallback);
+
+$http.delete(‘/someUrl’, config).then(successCallback, errorCallback);

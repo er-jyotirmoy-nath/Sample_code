f752d866a19690cd832b6eb86a003272e17f8cf0
Jyotirmoy Nath <er.jyotirmoy.nath@gmail.com>
2017-04-28 18:32:59 +0530

    Create angular_regex_email.js

---
+++ angular_regex_email.js
@@ -0,0 +1,2 @@
+<input type="text" class="form-control" name="emailId" ng-trim="false" ng-model="register.emailId" 
+									ng-pattern='/^(([^\s<>#!@$%=^~&`*+|}{":?()\[\]\\.,;:\s@"]+(\.[^\s<>#!@=$~%^&~`()\[\]\\.,;:\s@"]+)*))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/' required>
